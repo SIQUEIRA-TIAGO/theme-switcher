fc34e7d44706ce1d6b9f8314df411209aa5eed88
Tiago Oliveira <tiago.oliveira@dataunique.com.br>
2023-03-30 19:32:42 -0300

    using LocalStorage to save user preference

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-document.getElementsByTagName('input')[0].addEventListener('change', () => {
+const switchTheme = (switchBtn) => {
     const body = document.getElementsByTagName('body')[0]
-        body.classList.toggle('dark')
-        body.classList.toggle('light')
+    if(switchBtn){
+        localStorage.setItem('theme', 'dark')
+        body.classList.add('dark')
+        body.classList.remove('light')
+    } else {
+        localStorage.removeItem('theme')
+        body.classList.add('light')
+        body.classList.remove('dark')
+    }
+}
+
+const theme = localStorage.getItem('theme')
+if(theme){
+    switchTheme(document.getElementsByTagName('input')[0].checked = true)
+}
+
+document.getElementsByTagName('input')[0].addEventListener('change', (evt) => {
+    switchTheme(evt.target.checked)
 })
